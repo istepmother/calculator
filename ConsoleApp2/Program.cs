f513e805cc9c7725d4f10da0960a069877818d58
istepmother <imachukha@kse.org.ua>
2023-03-07 15:13:34 +0100

    Initital commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,52 @@
+﻿// namespace ConsoleApp2;
+
+using ConsoleApp2;
+using static ConsoleApp2.ArrayList;
+
+string ReadExpr()
+{
+    Console.Write("Enter the math expression: ");
+    return Console.ReadLine()!;
+}
+
+ArrayList Tokenize(string expression)
+{
+    string buffer = "";
+    ArrayList result = new ArrayList();
+
+    foreach (char s in expression)
+    {
+        if (Char.IsNumber(s))
+        {
+            buffer += s;
+        }
+        
+        else if (s == '+' || s == '-' || s == '*' || s == '/' || s == '^' || s == '(' || s == ')')
+        {
+            if (buffer.Length > 0)
+            {
+                result.Add(buffer);
+                buffer = "";
+            }
+            result.Add(s.ToString());
+        }
+    }
+
+    if (buffer.Length > 0)
+    {
+        result.Add(buffer);
+    }
+
+    return result;
+}
+
+// Main
+
+string input = ReadExpr();
+ArrayList tokens = Tokenize(input);
+
+// Console.WriteLine(string.Join("|", tokens));
+for (int index = 0; index < tokens.Count(); index++)
+{
+    Console.WriteLine(tokens.GetAt(index));
+}
